0b7d0847da333657679ef7595a82d1d014968bb3
LIUJE7 <LIUJE7@oocl.cpm>
2019-07-21 19:43:51 +0800

    [Jeffrey][Reconstruct the writing of the variable name in LineItem, Order, OredrReceipt; reconstruct the original too cumbersome processing, and specifically extract the method reconstruction.]

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -1,40 +1,47 @@
 package org.katas.refactoring;
 public class OrderReceipt {
-    private Order o;
-    public OrderReceipt(Order o) {
-        this.o = o;
+    private Order order;
+    public static  final  double TAX_RATE=.10;
+    private double totalSaleTax=0d;
+    private  double totalCost=0d;
+    public OrderReceipt(Order order) {
+        this.order = order;
     }
-    public void printOrders(StringBuilder output ){
+    public void print(StringBuilder output ){
         output.append("======Printing Orders======\n");
     }
-    public void printAttributes(StringBuilder output){
-        output.append(o.getCustomerName());
-        output.append(o.getCustomerAddress());
+    public void printAttributes(StringBuilder output) {
+        output.append(order.getCustomerName());
+        output.append(order.getCustomerAddress());
     }
-    public void printLineItem(StringBuilder output){
-        double totSalesTx=0;
-        double tot=0;
-        for (LineItem lineItem : o.getLineItems()) {
-            output.append(lineItem.getDescription());
-            output.append('\t');
-            output.append(lineItem.getPrice());
-            output.append('\t');
-            output.append(lineItem.getQuantity());
-            output.append('\t');
-            output.append(lineItem.totalAmount());
-            output.append('\n');
-            double salesTax = lineItem.totalAmount() * .10;
-            totSalesTx+= salesTax;
-            tot += lineItem.totalAmount() + salesTax;
+
+    public void printLineItems(StringBuilder output){
+        for (LineItem lineItem:order.getLineItems()) {
+            printOrder(output,lineItem);
+            double salesTax=lineItem.totalAmount()*TAX_RATE;
+            totalSaleTax+=salesTax;
+            totalCost+=lineItem.totalAmount()+salesTax;
         }
-        output.append("Sales Tax").append('\t').append(totSalesTx);
-        output.append("Total Amount").append('\t').append(tot);
+
     }
+    public void printOrder(StringBuilder output,LineItem lineItem){
+        output.append(lineItem.getDescription());
+        output.append('\t');
+        output.append(lineItem.getPrice());
+        output.append('\t');
+        output.append(lineItem.getQuantity());
+        output.append('\t');
+        output.append(lineItem.totalAmount());
+        output.append('\n');
+    }
+
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
-        printOrders(output);
+        print(output);
         printAttributes(output);
-        printLineItem(output);
+        printLineItems(output);
+        output.append("Sales Tax").append('\t').append(totalSaleTax);
+        output.append("Total Amount").append('\t').append(totalCost);
         return output.toString();
     }
 }
